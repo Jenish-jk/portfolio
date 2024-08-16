b0bb8783ec1abc481e3a90d86a89f8ff6480ae56
jenish-at-147099166213 <jenish.j@althisolutions.com>
2024-08-16 16:21:21 +0530

    changes in App.jsx

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,38 +1,28 @@
-
-import styles from './App.module.css'
-import { Hero } from "./components/Hero/Hero"
-import { Navbar } from './components/Navbar/Navbar'
-import { About } from './components/About/About'
-import { Experience } from './components/Experience/Experience'
-import { Projects } from './components/Projects/Projects'
-import { Contact } from './components/Contact/Contact'
-import {BrowserRouter,Routes,Route} from 'react-router-dom'
-
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Hero } from "./components/Hero/Hero";
+import { Navbar } from './components/Navbar/Navbar';
+import { About } from './components/About/About';
+import { Experience } from './components/Experience/Experience';
+import { Projects } from './components/Projects/Projects';
+import { Contact } from './components/Contact/Contact';
+import styles from './App.module.css';
 
 function App() {
-  
-
   return (
-    <div className = {styles.App}>
+    <div className={styles.App}>
       <BrowserRouter>
-      <Routes>
-        <Route path='/' element={ <Navbar />} />
-        <Route path='/hero' element={ <Hero />} />
-        <Route path='/about' element={ <About />} />
-        <Route path='/experience' element={ <Experience />} />
-        <Route path='/projects' element={ <Projects />} />
-        <Route path='/contact' element={ <Contact />} />
-      </Routes>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Hero />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/experience' element={<Experience />} />
+          <Route path='/projects' element={<Projects />} />
+          <Route path='/contact' element={<Contact />} />
+        </Routes>
       </BrowserRouter>
-   
-    
-    
-    
-    
-    
- 
     </div>
- )     
+  );
 }
 
-export default App
+export default App;
