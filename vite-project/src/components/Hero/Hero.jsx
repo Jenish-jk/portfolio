9b050a6408bb0611b464846659591a3bacca4a65
jenish-at-147099166213 <jenish.j@althisolutions.com>
2024-08-17 11:08:15 +0530

    changes in hero section

--- vite-project/src/components/Hero/Hero.jsx
+++ vite-project/src/components/Hero/Hero.jsx
@@ -12,7 +12,7 @@ export const Hero = () => {
         <p className={styles.description}>I'm A Full Stack Developer. Reach Out If You Like To Learn More.</p>
         <a href="mailto:suresh04102016@gmail.com" className={styles.contactBtn}>Contact Me</a>
       </div>
-      <img src={getImageUrl("hero/heroImage.png")} alt="Him" className = {styles.heroImg} />
+      <img src={getImageUrl("hero/me.jpg")} alt="Him" className = {styles.heroImg} />
       <div className={styles.topBlur} />
       <div className={styles.bottomBlur} />
     </section>
